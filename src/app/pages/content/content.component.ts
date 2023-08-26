ff74b9d930b5ce4d67785d59f3fde8bf89349271
Guilherme <guilhermerblc@gmail.com>
2023-08-26 18:41:09 -0300

    Content page load dynamic data.

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class ContentComponent {
 
+  photoCover:string = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png";
+  contentTitle:string = "Meu artigo";
+  contentDescription:string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quos excepturi ab alias sed numquam. Aliquid quisquam mollitia, vero pariatur quod cupiditate asperiores veniam iusto quis dolorem sint facilis velit?";
+
 }
